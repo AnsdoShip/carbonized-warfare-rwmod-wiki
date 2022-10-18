5897331a8ff1f7f921156c2cdd34a31f215f888e
tianscar <tianscar@protonmail.com>
2022-10-19 00:12:01 +0800

    Wiki updated: 2022-10-19 00:12:01

--- publish.sh
+++ publish.sh
@@ -6,10 +6,4 @@ git commit -m "Wiki updated: $(date "+%Y-%m-%d %H:%M:%S")"
 git pull origin main
 git push origin main
 
-mkdocs build
-cp docs/README.md site/
-cd site
-git checkout gh-pages
-git add .
-git commit -m "Wiki updated: $(date "+%Y-%m-%d %H:%M:%S")"
-git push origin gh-pages
+mkdocs gh-deploy
